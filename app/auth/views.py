d8d319398f30df3bf97e5806d4e89484199659af
Valeriy Ilihmetov <valeriy.ilihmetov@dev-pro.net>
2015-10-13 18:04:50 +0300

    Add changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models.user import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -23,3 +24,16 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('you can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
